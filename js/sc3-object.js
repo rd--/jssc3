d1cb9835dc9ddcd108b5907e46374405232d3e5f
rohan <rd@slavepianos.org>
2022-02-15 12:45:34 +1100

    +copy_keys_from_object

--- js/sc3-object.js
+++ js/sc3-object.js
@@ -4,3 +4,11 @@
 function objectKeyFromValue(object, value) {
   return Object.keys(object).find(key => object[key] === value);
 }
+
+// Make a new object having only the indicated fields copied from the input object.
+// copy_keys_from_object({a: 1, b: 2, c: 3}, ['a', 'c']) // => {a: 1, c: 3}
+function copy_keys_from_object(object, keysArray) {
+    var answer = {};
+    keysArray.forEach(key => answer[key] = object[key]);
+    return answer;
+}
