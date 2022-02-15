b2fb8da24fb654850d9659199629973d65287e6a
rohan <rd@slavepianos.org>
2022-02-15 23:11:04 +1100

    +fetch_soundfile_channels_to_scsynth_buffer

--- js/sc3-buffer.js
+++ js/sc3-buffer.js
@@ -1,14 +1,36 @@
 'use strict';
 
-// fetch_soundfile_to_scsynth_buffer(0, 'https://rohandrape.net/rd/j/2022-02-15/harp-a4.flac')
-function fetch_soundfile_to_scsynth_buffer(bufferNumber, soundFileUrl) {
+function audiobuffer_to_scsynth_buffer(audioBuffer, bufferNumber, numberOfChannels, bufferData) {
+    var numberOfFrames = audioBuffer.length;
+    var sampleRate = audioBuffer.sampleRate;
+    var oscMessage = b_alloc_then_memcpy(bufferNumber, numberOfFrames, numberOfChannels, sampleRate, encodeFloat32Array(bufferData));
+    console.log('audiobuffer_to_scsynth_buffer', oscMessage);
+    sendOsc(oscMessage);
+}
+
+// Fetch sound file data, and then allocate a buffer and memcpy all channel data.
+function fetch_soundfile_to_scsynth_buffer(soundFileUrl, bufferNumber) {
     fetch_soundfile_to_audiobuffer_and_then(soundFileUrl, function (audioBuffer) {
-        var numberOfFrames = audioBuffer.length;
-        var numberOfChannels = audioBuffer.numberOfChannels;
-        var sampleRate = audioBuffer.sampleRate;
-        var bufferData = encodeFloat32Array(audiobuffer_interleaved_channel_data(audioBuffer));
-        var oscMessage = b_alloc_then_memcpy(bufferNumber, numberOfFrames, numberOfChannels, sampleRate, bufferData);
-        console.log('fetch_soundfile_to_scsynth_buffer', oscMessage);
-        sendOsc(oscMessage);
+        audiobuffer_to_scsynth_buffer(
+            audioBuffer,
+            bufferNumber,
+            audioBuffer.numberOfChannels,
+            audiobuffer_interleaved_channel_data(audioBuffer)
+        );
+    });
+}
+
+// Fetch single channel of sound file data.  The channel number is one-indexed.
+function fetch_soundfile_channels_to_scsynth_buffer(soundFileUrl, bufferNumbers, channelIndices) {
+    fetch_soundfile_to_audiobuffer_and_then(soundFileUrl, function (audioBuffer) {
+        for(var i = 0; i < bufferNumbers.length; i++) {
+            var bufferNumber = bufferNumbers[i];
+            var channelIndex = channelIndices[i];
+            if(channelIndex >= 1 && channelIndex <= audioBuffer.numberOfChannels) {
+                audiobuffer_to_scsynth_buffer(audioBuffer, bufferNumber, 1, audioBuffer.getChannelData(channelIndex - 1));
+            } else {
+                console.error('fetch_soundfile_channel_to_scsynth_buffer: channelIndex out of bounds', channelIndex, audioBuffer.numberOfChannels);
+            };
+        };
     });
 }
