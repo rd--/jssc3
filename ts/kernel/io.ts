310296aecebf951eaa3010ae1d6569803884548f
rohan <rd@slavepianos.org>
2022-11-10 11:36:41 +1100

    +load_utf8

--- ts/kernel/io.ts
+++ ts/kernel/io.ts
@@ -1,16 +1,36 @@
+import { consoleDebug } from './error.js'
+
 // Append timestamp to URL to defeat cache
 export function url_append_timestamp(url: string): string {
 	const ext = ((/\?/).test(url) ? '&': '?') + (new Date()).getTime();
 	return url + ext;
 }
 
+export function fetch_url<T>(url: string, responseType: XMLHttpRequestResponseType): Promise<T> {
+	consoleDebug(`fetch_url: ${url}`);
+    return new Promise(function (resolve, reject) {
+		const request = new XMLHttpRequest();
+        request.open('GET', url);
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 300) {
+                resolve(request.response);
+            } else {
+                reject(request.statusText);
+            }
+        };
+        request.onerror = function() {
+			reject(request.statusText);
+		};
+		request.responseType = responseType;
+        request.send();
+    });
+}
+
 // Fetch url with indicated responseType and run proc asynchronously on result.
-export function fetch_url_and_then<T>(url: string, responseType: XMLHttpRequestResponseType, proc: (x: T) => void): void {
-	const request = new XMLHttpRequest();
-	request.addEventListener('load', () => proc(request.response));
-	request.open('GET', url);
-	request.responseType = responseType;
-	request.send();
+export function fetch_url_and_then<T>(url: string, responseType: XMLHttpRequestResponseType, proc: (reply: T) => void): void {
+	fetch_url(url, responseType)
+		.then(answer => proc(<T>answer))
+		.catch(reason => console.error(`fetch_url_and_then: ${url}: ${reason}`));
 }
 
 // Throw error if response status is not .ok
@@ -71,4 +91,11 @@ export function read_text_file_from_file_input_and_then(inputId: string, fileInd
 // Read json file and run proc on parsed result.
 export function read_json_file_and_then(jsonFile: File , proc: (aValue: Record<string, unknown> | []) => void): void {
 	read_text_file_and_then(jsonFile, jsonText => proc(JSON.parse(jsonText)));
+}
+
+export function load_utf8(url: string): Promise<string> {
+	return fetch(url, { cache: 'no-cache' })
+		.then(response => handle_fetch_error(response))
+		.then(response => response.text())
+		.catch(reason => `load_utf8: ${url}: ${reason}`);
 }
