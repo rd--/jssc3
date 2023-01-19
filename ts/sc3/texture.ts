500b841131e755ad6707a4aef26e687e8930a242
rohan <rd@slavepianos.org>
2023-01-19 12:02:47 +1100

    +XFadeTexture

--- ts/sc3/texture.ts
+++ ts/sc3/texture.ts
@@ -14,3 +14,15 @@ export function OverlapTexture(graphFunc: (tr: Signal) => Signal, sustainTime: n
 	};
 	return arrayReduce(arrayMap(voiceFunction, arrayFromTo(0, overlap - 1)), Add);
 }
+
+export function XFadeTexture(graphFunc: (tr: Signal) => Signal, sustainTime: number, transitionTime: number): Signal {
+	const envDur = (sustainTime + transitionTime) * 2;
+	const voiceFunction = function(phase: number): Signal {
+		const trg = kr(Impulse(1 / envDur, phase));
+		const snd = graphFunc(trg);
+		const env = new Env([0, 1, 1, 0, 0], [transitionTime, sustainTime, transitionTime, sustainTime], 'sin', null, null, 0);
+		const sig = Mul(snd, EnvGen(trg, 1, 0, 1, 0, envCoord(env)));
+		return sig;
+	};
+	return Add(voiceFunction(0), voiceFunction(0.5))
+}
