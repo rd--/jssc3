fc68a28e991740a081a03c93625cde8ca0bbf038
rohan <rd@slavepianos.org>
2023-02-19 08:48:21 +1100

    ~sk,+fonts

--- sh/help-smallhours.sh
+++ sh/help-smallhours.sh
@@ -3,6 +3,6 @@
 cd ~/sw/jssc3/text/
 rm -f smallhours-help.text
 (cd ~/sw/spl/ ; ls -1 help/spl/*.sl) >> smallhours-help.text
-(cd ~/sw/spl/ ; ls -1 doc/spl/*.sl) >> smallhours-help.text
+(cd ~/sw/spl/ ; ls -1 doc/*/*.sl) >> smallhours-help.text
 (cd ~/sw/stsc3/ ; ls -1 help/sc/*.sl) >> smallhours-help.text
 (cd ~/sw/stsc3/ ; ls -1 doc/sc/*.sl) >> smallhours-help.text
