d9b5c65baa377d4fe1b8db538977d792f5d7bab0
rohan <rd@slavepianos.org>
2023-02-28 12:38:27 +1100

    ~ts,+primes

--- ts/kernel/number.ts
+++ ts/kernel/number.ts
@@ -35,3 +35,30 @@ export function numberTimesRepeat(count: number, proc: (aValue: void) => void): 
 export function numberToString(aNumber: number): string {
 	return Number(aNumber).toString();
 }
+
+export function isPrime(aNumber: number): boolean {
+	if (aNumber <= 1) return false;
+	if (aNumber === 2) return true;
+	const sqrt = Math.sqrt(aNumber);
+	for (let i = 2; i <= sqrt; i++) {
+		if (aNumber % i === 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+export function primeFactors(aNumber: number): number[] {
+	const factors = [];
+	let divisor = 2;
+	let n = aNumber;
+	while (n >= 2) {
+		if (n % divisor == 0) {
+			factors.push(divisor);
+			n = n / divisor;
+		} else {
+			divisor++;
+		}
+	}
+	return factors;
+}
