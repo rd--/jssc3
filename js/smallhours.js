bdba6537fe55c1daa56748527420be2dac2f26c2
rohan <rd@slavepianos.org>
2023-09-09 14:50:32 +1000

    ~spl

--- js/smallhours.js
+++ js/smallhours.js
@@ -17,9 +17,9 @@ export const state = { autoPlay: false, oracleFiles: null };
 
 function clear() {
 	if(sl.slOptions.simpleArityModel) {
-		_clear(_workspace.get('clock'));
+		_removeAll(_at(_system, 'clock'));
 	} else {
-		_clear_1(_workspace.get('clock'));
+		_removeAll_1(_at_2(_system, 'clock'));
 	}
 }
 
