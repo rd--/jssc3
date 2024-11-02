3bd2d1e509bd96c9efbdd154e172cdf19051fabd
rohan <rd@slavepianos.org>
2024-11-03 09:15:48 +1100

    ~sh

--- sh/remote-update.sh
+++ sh/remote-update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-(cd ~/sw/jssc3 ; pwd ; make push-all remote-update)
-(cd ~/sw/blksc3 ; pwd ; make push-all remote-update)
-(cd ~/sw/spl ; pwd ; make push-all remote-update)
+(cd ~/sw/jssc3 ; pwd ; make push-all)
+(cd ~/sw/blksc3 ; pwd ; make push-all)
+(cd ~/sw/spl ; pwd ; make push-all)
+
+ssh rd@rohandrape.net "(cd ~/rohandrape.net/pub/jssc3; git pull; make remote-setup; cd ~/rohandrape.net/pub/blksc3; git pull; make remote-setup; cd ~/rohandrape.net/pub/spl; git pull)"
