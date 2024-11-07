303fc9656b76079ff7c405c3b37fbdd027101ecd
rohan <rd@slavepianos.org>
2024-11-08 07:09:07 +1100

    +inverseFft

--- ts/stdlib/fft.ts
+++ ts/stdlib/fft.ts
@@ -1,10 +1,20 @@
 import FFT from '../../lib/scsynth-wasm-builds/lib/ext/fft.js';
 
+// input is real, output is complex [r1, i1, r2, i2 ...]
 export function realFft(input: number[]): number[] {
 	const size = input.length;
 	const fft = new FFT(size);
 	const output = fft.createComplexArray();
 	fft.realTransform(output, input);
 	fft.completeSpectrum(output);
+	return output;
+}
+
+// input and output are both complex
+export function inverseFft(input: number[]): number[] {
+	const size = input.length / 2;
+	const fft = new FFT(size);
+	const output = fft.createComplexArray();
+	fft.inverseTransform(output, input);
 	return output;
 }
