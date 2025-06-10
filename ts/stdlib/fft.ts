cd396f37ff612a3aebbcc611496efad3e47bda9a
rohan <rd@slavepianos.org>
2025-06-10 12:52:41 +1000

    ~fft

--- ts/stdlib/fft.ts
+++ ts/stdlib/fft.ts
@@ -1,6 +1,15 @@
 import FFT from '../../lib/scsynth-wasm-builds/lib/ext/fft.js';
 
-// input is real, output is complex [r1, i1, r2, i2 ...]
+// input and output are both complex [r1, i1, r2, i2 ...]
+export function fft(input: number[]): number[] {
+	const size = input.length / 2;
+	const fft = new FFT(size);
+	const output = fft.createComplexArray();
+	fft.transform(output, input);
+	return output;
+}
+
+// input is real [r1 r2 ...], output is complex [r1, i1, r2, i2 ...]
 export function realFft(input: number[]): number[] {
 	const size = input.length;
 	const fft = new FFT(size);
